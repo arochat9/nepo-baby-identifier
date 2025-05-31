5ca6bcac039bba7f9f1f0748911eae248c0ecc16
Andrew Rochat <arochat@palantir.com>
2025-05-31 16:02:30 -0400

    adding output dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
